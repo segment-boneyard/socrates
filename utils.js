3ff5551a5c5adb2193ed2a1db9353517f5736d40
Ilya Volodarsky <ivolo@mit.edu>
2012-11-10 15:52:19 -0800

    adding basic saving functionality

---
+++ utils.js
@@ -0,0 +1,27 @@
+
+// from http://diveintohtml5.info/storage.html
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+function guid ()
+{
+    var S4 = function ()
+    {
+        return Math.floor(
+                Math.random() * 0x10000 /* 65536 */
+            ).toString(16);
+    };
+
+    return (
+            S4() + S4() + "-" +
+            S4() + "-" +
+            S4() + "-" +
+            S4() + "-" +
+            S4() + S4() + S4()
+        );
+}
