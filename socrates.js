afb4b2748a72070b638a9ac8b5d0a07551c483ca
Ian Storm Taylor <ian@ianstormtaylor.com>
2012-12-12 04:25:32 -0800

    changed to analytics.io

--- socrates.js
+++ socrates.js
@@ -20,15 +20,3 @@ $(function () {
 
     Backbone.history.start();
 });
-
-
-// Analytics
-// ---------
-
-var dev = window.location.hostname === 'localhost';
-window.analytics.initialize({
-    'Segment.io'       : dev ? 'r12y067n' : 'r3gxgjte',
-    'Google Analytics' : dev ? ''         : 'UA-36265018-1',
-    'Mixpanel'         : dev ? ''         : '78d583c1ec60a099cf3496506f73d29c',
-    'KISSmetrics'      : dev ? ''         : '9ce3ea2509ea5f60b82718508c77a16298e92d2a'
-});
