ad4f8561781dd709594d3f4270506cef9b2b400b
Peter Reinhardt <peter@segment.io>
2013-03-03 14:34:03 -0800

    reversing

--- app/app-model.js
+++ app/app-model.js
@@ -89,9 +89,9 @@ Socrates.Model = Backbone.Model.extend({
     // --------------
 
     onHomeRoute : function () {
-        var body = onboarding;
-        if (localStorage.getItem(this.bookmarkKey))
-            body = ''; // blank slate for an experienced user
+        var body = ''; // blank slate for an experienced user
+        if (!localStorage.getItem(this.bookmarkKey))
+            body = onboarding; // onboarding text for a new user 
 
         this.set('document', this.createDocument(body));
 
