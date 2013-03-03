bf80c7a48aceea58450b93a4f7780a6f49072882
Peter Reinhardt <peter@segment.io>
2013-03-03 14:26:45 -0800

    fixing blank slate

--- app/app-model.js
+++ app/app-model.js
@@ -90,10 +90,8 @@ Socrates.Model = Backbone.Model.extend({
 
     onHomeRoute : function () {
         var body = onboarding;
-        if (localStorage.getItem(this.bookmarkKey)) {
-            var index = _.random(0, random.length-1);
-            body = random[index];
-        }
+        if (localStorage.getItem(this.bookmarkKey))
+            body = ''; // blank slate for an experienced user
 
         this.set('document', this.createDocument(body));
 
