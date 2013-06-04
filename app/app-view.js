70f70b34f5463a351fd15ad4b7147d73ba776d54
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-06-03 19:49:55 -0700

    think i got all the js down and cleaned now

--- app/app-view.js
+++ app/app-view.js
@@ -1,207 +1,25 @@
-/*global Socrates Backbone marked _ Rainbow */
-
-var $window = $(window);
-var MININUM_WIDTH = 1000;
+// Allow tabs in the textarea using a jQuery plugin.
+this.$textarea.tabby({tabString:'    '});
 
 
-Socrates.View = Backbone.View.extend({
+onAddButtonClick : function (event) {
+    this.model.set('document', this.model.createDocument());
+},
 
-    youtubeEmbedTemplate : _.template('<iframe width="100%" height="400" src="http://www.youtube.com/embed/<%= id %>" frameborder="0" allowfullscreen></iframe>'),
+onReadOnlyButtonClick : function (event) {
+    event.preventDefault();
 
-    events : {
-        'keyup .document-textarea' : 'onTextareaKeyup',
-        'click .read-only-button'  : 'onReadOnlyButtonClick',
-        'click .write-only-button' : 'onWriteOnlyButtonClick',
-        'click .add-button'        : 'onAddButtonClick',
-        'click .menu-button'       : 'onMenuButtonClick'
-    },
+    var state = this.$readOnlyButton.state('pressed') ? null : 'read';
+    this.model.set('state', state);
 
-    initialize : function (options) {
-        _.bindAll(this);
+    window.analytics.track('Press Read-only Button');
+},
 
-        // Cache some jQuery selectors.
-        this.$title           = this.$('title');
-        this.$menu            = this.$('.document-menu');
-        this.$textarea        = this.$('.document-textarea');
-        this.$article         = this.$('.document-article');
-        this.$menuButton      = this.$('.menu-button');
-        this.$readOnlyButton  = this.$('.read-only-button');
-        this.$writeOnlyButton = this.$('.write-only-button');
+onWriteOnlyButtonClick : function (event) {
+    event.preventDefault();
 
-        // Allow tabs in the textarea using a jQuery plugin.
-        this.$textarea.tabby({tabString:'    '});
+    var state = this.$writeOnlyButton.state('pressed') ? null : 'write';
+    this.model.set('state', state);
 
-        // Make a menu to select documents from.
-        this.documentMenu = new Socrates.DocumentMenuView({
-            collection : this.model.get('documents'),
-            el         : this.$menu
-        })
-            .on('select', this.onDocumentMenuSelect);
-
-        // Attach app event handlers.
-        this.model
-            .on('change:document', this.onAppDocumentChange)
-            .on('change:state', this.renderState);
-        // Attach document event handlers.
-        if (this.model.has('document')) this.applyDocumentEventHandlers(this.model.get('document'));
-
-        // Throttle saving to not hit firebase so much.
-        this.save = _.debounce(this.save, 500);
-        // Debounce applying the youtube filter since it's kinda intensive.
-        this.renderYoutubeFilter = _.debounce(this.renderYoutubeFilter, 1000);
-
-        // Add a window resize handler to re-try state.
-        $window.on('resize', this.onWindowResize);
-    },
-
-    renderArticle : function () {
-        if (!this.model.has('document')) return this;
-
-        // Convert the model's markdown body into html.
-        var body     = this.model.get('document').get('body');
-        var markdown = marked(body);
-        var quoted   = this.renderSmartQuoteFilter(markdown);
-        this.$article.html(quoted);
-
-        // Apply extra filters.
-        this.renderYoutubeFilter();
-        this.renderCodeHighlightingFilter();
-        this.renderMathJax();
-    },
-
-    // Turn dumb quotes into smart quotes.
-    renderSmartQuoteFilter : function (markdown) {
-        // Left quotes are either next to a space or code stuff.
-        var leftSingleQuote = /([^\w])&#39;/g;
-        var leftDoubleQuote = /([^\w])&quot;/g;
-        // Right quotes are everything else...
-        var singleQuote = /&#39;/g;
-        var doubleQuote = /&quot;/g;
-
-        markdown = markdown
-            .replace(leftSingleQuote, '$1&lsquo;')
-            .replace(leftDoubleQuote, '$1&ldquo;')
-            .replace(singleQuote, '&rsquo;')
-            .replace(doubleQuote, '&rdquo;');
-
-        return markdown;
-    },
-
-    // Create embeds for any youtube links.
-    renderYoutubeFilter : function () {
-        var self = this;
-        this.$article.find('a[href*="youtube.com/watch?v="]').each(function (i, el) {
-            var youtubeId = el.href.match(/\?v=([\w-]+)/)[1];
-            var embed     = self.youtubeEmbedTemplate({ id : youtubeId });
-            $(el).replaceWith(embed);
-
-            window.analytics.track('Render Youtube Video', {
-                video : youtubeId
-            });
-        });
-    },
-
-    // Tell mathjax to do it's thing
-    renderMathJax : _.debounce(function () {
-        MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-    }, 150),
-
-    // Apply code highlighting. We have to convert the highlighting classes
-    // that marked.js gives us into ones that Rainbow.js can read first.
-    renderCodeHighlightingFilter : function () {
-        this.$article.find('code').each(function (index, el) {
-            var $el = $(el);
-
-            // Convert any smart quotes into dumb quotes. This is janky.
-            var html = $el.html();
-            var leftSingleQuote = /‘/g;
-            var leftDoubleQuote = /“/g;
-            var singleQuote     = /’/g;
-            var doubleQuote     = /”/g;
-
-            html = html
-                .replace(leftSingleQuote, '\'')
-                .replace(leftDoubleQuote, '\"')
-                .replace(singleQuote, '\'')
-                .replace(doubleQuote, '\"');
-
-            $el.html(html);
-
-            var classes = el.className.split(/\s+/);
-            _.each(classes, function (className, i) {
-                if (className.indexOf('lang-') !== -1) {
-                    var language = className.substring('lang-'.length);
-                    $el.attr('data-language', language);
-
-                    window.analytics.track('Render Code Highlighting', {
-                        language : language
-                    });
-                }
-            });
-        });
-        try { Rainbow.color(); } catch (e) {}
-    },
-
-    toggleMenu : function () {
-        this.$menu.slideToggle();
-        this.$menuButton.toggleState('pressed');
-    },
-
-
-    // Event Handlers
-    // --------------
-
-    // When the window resizes too small, if we're not in one of the two states,
-    // force us into write-only mode.
-    onWindowResize : function (event) {
-        if (this.model.has('state')) return;
-
-        if ($window.width() < MININUM_WIDTH) this.model.set('state', 'write');
-    },
-
-    onMenuButtonClick : function (event) {
-        event.preventDefault();
-
-        this.toggleMenu();
-    },
-
-    onAddButtonClick : function (event) {
-        this.model.set('document', this.model.createDocument());
-    },
-
-    onReadOnlyButtonClick : function (event) {
-        event.preventDefault();
-
-        var state = this.$readOnlyButton.state('pressed') ? null : 'read';
-        this.model.set('state', state);
-
-        window.analytics.track('Press Read-only Button');
-    },
-
-    onWriteOnlyButtonClick : function (event) {
-        event.preventDefault();
-
-        var state = this.$writeOnlyButton.state('pressed') ? null : 'write';
-        this.model.set('state', state);
-
-        window.analytics.track('Press Write-only Button');
-    },
-
-    onAppDocumentChange : function (model, document) {
-        var previousDocument = this.model.previous('document');
-        if (previousDocument) this.applyDocumentEventHandlers(previousDocument, true);
-        this.applyDocumentEventHandlers(document);
-
-        this.renderTextarea()
-            .renderArticle();
-    },
-
-    onDocumentMenuSelect : function (menu, document) {
-        this.model.set('document', document);
-
-        window.analytics.track('Select a Document', {
-            id : document.id
-        });
-    }
-
-});
+    window.analytics.track('Press Write-only Button');
+},
